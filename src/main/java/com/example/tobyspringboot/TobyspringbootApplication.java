5ce4aefb91c860d6c5bce8d56a26d740b2e657fb
simsimeie <simsimeie@naver.com>
2023-03-30 23:43:06 +0900

    ContainerLess#1-서블릿 컨테이너 및 서블릿 생성

---
+++ src/main/java/com/example/tobyspringboot/TobyspringbootApplication.java
@@ -0,0 +1,35 @@
+package com.example.tobyspringboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TobyspringbootApplication {
+
+    public static void main(String[] args){
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext->{
+            servletContext.addServlet("hello", new HttpServlet(){
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 서비스 구현
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+
+        });
+        webServer.start();
+    }
+
+}
