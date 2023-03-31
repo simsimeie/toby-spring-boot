f27703226a1859aca401e7033c2d9906e533b2ba
simsimeie <simsimeie@naver.com>
2023-03-31 11:21:50 +0900

    ContainerLess#5-Dispatcher Servlet 사용

--- src/main/java/com/example/tobyspringboot/HelloController.java
+++ src/main/java/com/example/tobyspringboot/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.tobyspringboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
     public HelloController(HelloService helloService){
         this.helloService = helloService;
     }
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name){
         return helloService.hello(Objects.requireNonNull(name));
     }
--- src/main/java/com/example/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobyspringbootApplication.java
@@ -3,23 +3,13 @@ package com.example.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class TobyspringbootApplication {
 
     public static void main(String[] args){
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         // 인터페이스 타입이 아닌 구체클래스 타입을 명시해야 한다.
         applicationContext.registerBean(SimpleHelloService.class);
@@ -27,24 +17,10 @@ public class TobyspringbootApplication {
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext->{
-            servletContext.addServlet("frontcontroller", new HttpServlet(){
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    }
-                    else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                    // DispatcherServlet에는 Web용 ApplicationContext를 파라미터로 제공해야한다.
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
 
         });
         webServer.start();
