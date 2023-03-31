cb34fbb2908039976e96492d7fbb0fb7fcbc1f78
simsimeie <simsimeie@naver.com>
2023-03-31 12:32:40 +0900

    ContainerLess#10-MySpringBootApplication 클래스로 분리하여 사용

--- src/main/java/com/example/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobyspringbootApplication.java
@@ -23,24 +23,7 @@ public class TobyspringbootApplication {
     }
 
     public static void main(String[] args){
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh(){
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext->{
-                    servletContext.addServlet("dispatcherServlet",dispatcherServlet)
-                            .addMapping("/*");
-
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(TobyspringbootApplication.class);
-        applicationContext.refresh();
+        MySpringBootApplication.run(TobyspringbootApplication.class, args);
     }
 
 }
