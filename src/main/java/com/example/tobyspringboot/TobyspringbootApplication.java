c1ecaf75f26c6afa216d299648efe45ae04853b1
simsimeie <simsimeie@naver.com>
2023-03-31 11:59:55 +0900

    ContainerLess#6-ApplicationContext refresh시점에 서블릿 컨테이너 기동(내장 톰캣 원리)

--- src/main/java/com/example/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobyspringbootApplication.java
@@ -9,21 +9,26 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class TobyspringbootApplication {
 
     public static void main(String[] args){
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh(){
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext->{
+                    servletContext.addServlet("dispatcherServlet",
+                            // DispatcherServlet에는 Web용 ApplicationContext를 파라미터로 제공해야한다.
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+
+                });
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         // 인터페이스 타입이 아닌 구체클래스 타입을 명시해야 한다.
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext->{
-            servletContext.addServlet("dispatcherServlet",
-                    // DispatcherServlet에는 Web용 ApplicationContext를 파라미터로 제공해야한다.
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-
-        });
-        webServer.start();
     }
 
 }
